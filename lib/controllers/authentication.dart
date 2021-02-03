6449f4e6f84e23003ef791419795375d54441e62
PilotJinix <agungsantoso557@gmail.com>
2021-02-04 03:19:04 +0700

    Login Ui

--- lib/controllers/authentication.dart
+++ lib/controllers/authentication.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Authentication with ChangeNotifier{
+
+  final String baseurl = '';
   Future<void> logIn(String email, String password) async{
+    final response = http.post("$baseurl/")
 
   }
 
