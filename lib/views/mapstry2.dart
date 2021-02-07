2e848581a4d0f9e4684931c546b1b6ad2b9a710c
PilotJinix <agungsantoso557@gmail.com>
2021-02-07 21:58:46 +0700

    Google Maps

--- lib/views/mapstry2.dart
+++ lib/views/mapstry2.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,13 +10,37 @@ class GoogleMaps extends StatefulWidget{
 }
 
 class _GoogleMapsState extends State<GoogleMaps> {
+
+  Set<Marker> _marker = HashSet<Marker>();
+  GoogleMapController _mapController;
+
+  void _onMapCreated(GoogleMapController googleMapController){
+    _mapController = googleMapController;
+
+    setState(() {
+      _marker.add(
+        Marker(
+          markerId: MarkerId("0"),
+          position: LatLng(-8.1417907, 113.7260868),
+          infoWindow: InfoWindow(title: "Lokasi Absen")
+        )
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
           target: LatLng(-8.141719, 113.726656),
         zoom: 15
-      )),
+      ),
+        markers: _marker,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
     );
   }
 }
