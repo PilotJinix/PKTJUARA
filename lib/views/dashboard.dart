9a2b3d980b61214b816789d424b56e2f2ad8e734
PilotJinix <agungsantoso557@gmail.com>
2021-02-08 12:01:47 +0700

    Time

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,11 +1,32 @@
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart';
+import 'package:pktjuara/service/world_time.dart';
 import 'package:pktjuara/views/mapstry2.dart';
 
-class dashboard extends StatelessWidget{
+class dashboard extends StatefulWidget{
 
+
+  @override
+  _dashboardState createState() => _dashboardState();
+}
+
+class _dashboardState extends State<dashboard> {
+
+  void setupTime()async{
+    WorldTime intance = WorldTime(location: "Bontang", flag: "Indonesia.png", url: "Asia/Jakarta");
+    await intance.getTime();
+    print(intance.time);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    setupTime();
+  }
 
   Widget header(){
     return ListTile(
@@ -74,11 +95,26 @@ class dashboard extends StatelessWidget{
           child: Column(
             children: [
               CardProfile(),
+              SizedBox(height: 15,),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                          "Form Absensi",
+                        style: TextStyle(
+                          fontSize: 20
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              )
             ],
           ),
         ),
       )
     );
   }
-
 }
