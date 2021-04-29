c8dbfce27d4850d2acab60f19d235b6fa537776d
PilotJinix <agungsantoso557@gmail.com>
2021-04-29 13:51:36 +0700

    loading done

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -20,6 +20,7 @@ import 'package:pktjuara/views/profile.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -32,6 +33,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController password = new TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey();
   bool loading = false;
+  DateTime currenttime;
 
   Future log()async{
     var data = new Map<String, dynamic>();
@@ -114,125 +116,145 @@ class _LoginPageState extends State<LoginPage> {
 
   }
 
+  Future<bool> pop(){
+    DateTime out = DateTime.now();
+    if (currenttime == null || out.difference(currenttime) > Duration(seconds: 2)){
+      currenttime = out;
+      FlutterToast.showToast(
+          msg: "Tekan lagi untuk keluar Aplikasi",
+          toastLength: Toast.LENGTH_SHORT,
+          textColor: Colors.white,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.grey
+      );
+      return Future.value(false);
+    }else{
+      exit(0);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return loading ? Loading() : Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: LoginColor(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text(
-                    "Masuk",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF0D6EAA),
-                        fontSize: 25
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-                SizedBox(height: size.height * 0.03),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: 40),
-                  child: TextFormField(
-                    controller: npk,
-                    validator: (value)
-                    {
-                      if(value.isEmpty)
-                      {
-                        return 'invalid Email';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Email"
-                    ),
-                  ),
-                ),
-                SizedBox(height: size.height * 0.03),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: 40),
-                  child: TextFormField(
-                    controller: password,
-                    validator: (value)
-                    {
-                      if(value.isEmpty)
-                      {
-                        return 'invalid Password';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Kata Sandi"
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    CoolAlert.show(context: context, type: CoolAlertType.warning, text: "Silakan Hubungi Developer", title: "Akses Ditolak");
-                  },
-                  child: Container(
-                    alignment: Alignment.centerRight,
-                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+    return WillPopScope(
+      onWillPop: ()=>pop(),
+      child: loading ? Loading() : Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: LoginColor(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 40),
                     child: Text(
-                      "Lupa Kata Sandi?",
+                      "Masuk",
                       style: TextStyle(
-                          fontSize: 12,
-                          color: Color(0XFF2661FA)
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0D6EAA),
+                          fontSize: 25
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 40),
+                    child: TextFormField(
+                      controller: npk,
+                      validator: (value)
+                      {
+                        if(value.isEmpty)
+                        {
+                          return 'invalid Email';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Email"
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: size.height * 0.05),
-                Container(
-                  alignment: Alignment.centerRight,
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                  child: RaisedButton(
-                    onPressed: () {
-                      log();
-                    },
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0),
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 50.0,
-                      width: size.width * 0.5,
-                      decoration: new BoxDecoration(
-                          borderRadius: BorderRadius.circular(80.0),
-                          gradient: new LinearGradient(
-                              colors: [
-                                Color.fromARGB(255, 255, 136, 34),
-                                Color.fromARGB(255, 255, 177, 41)
-                              ]
-                          )
+                  SizedBox(height: size.height * 0.03),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 40),
+                    child: TextFormField(
+                      controller: password,
+                      validator: (value)
+                      {
+                        if(value.isEmpty)
+                        {
+                          return 'invalid Password';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Kata Sandi"
                       ),
-                      padding: const EdgeInsets.all(0),
+                      obscureText: true,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      CoolAlert.show(context: context, type: CoolAlertType.warning, text: "Silakan Hubungi Developer", title: "Akses Ditolak");
+                    },
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
                       child: Text(
-                        "MASUK",
-                        textAlign: TextAlign.center,
+                        "Lupa Kata Sandi?",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold
+                            fontSize: 12,
+                            color: Color(0XFF2661FA)
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-      )
+                  SizedBox(height: size.height * 0.05),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    child: RaisedButton(
+                      onPressed: () {
+                        log();
+                      },
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                      textColor: Colors.white,
+                      padding: const EdgeInsets.all(0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 50.0,
+                        width: size.width * 0.5,
+                        decoration: new BoxDecoration(
+                            borderRadius: BorderRadius.circular(80.0),
+                            gradient: new LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 255, 136, 34),
+                                  Color.fromARGB(255, 255, 177, 41)
+                                ]
+                            )
+                        ),
+                        padding: const EdgeInsets.all(0),
+                        child: Text(
+                          "MASUK",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        )
+      ),
     );
   }
 }
